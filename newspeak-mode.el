ca8dbb408665ab32cebf3411921b874546ff992b
Frank Shearar <frank.shearar@gmail.com>
2011-12-05 17:09:47 +0000

    Turns out the latest Newspeak files are stored in .ns3 files. I don't plan to support old NS versions, so might as well be explicit about it.

--- newspeak-mode.el
+++ newspeak-mode.el
@@ -1,8 +1,8 @@
 ;;;###autoload
 (define-generic-mode newspeak-mode
-  '(("\"" . "\""))
+  '(("\"" . "\"") ("(*" . "*)"))
   '("self" "super" "outer" "true" "false" "nil" "thisContext")
   nil ; font-lock
-  '("\\.ns$") ; auto-mode-alist
+  '("\\.ns3$") ; auto-mode-alist
   nil ; function-list
   "A major mode for editing Newspeak.")
